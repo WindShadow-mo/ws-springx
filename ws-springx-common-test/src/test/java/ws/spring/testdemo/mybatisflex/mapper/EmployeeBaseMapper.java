c08a42f902cfcc5f95fd0f2a62b59b7f32dd31f4
WindShadow <WindShadow@ws.com>
2026-03-28 16:56:05 +0800

    feat: mybatis-flex适配SpringBoot4

---
+++ ws-springx-common-test/src/test/java/ws/spring/testdemo/mybatisflex/mapper/EmployeeBaseMapper.java
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2025 莫千风
+ */
+
+package ws.spring.testdemo.mybatisflex.mapper;
+
+import ws.spring.mybatisflex.GenericBaseMapper;
+import ws.spring.testdemo.pojo.Employee;
+
+/**
+ * @author WindShadow
+ * @version 2025-07-13.
+ */
+public interface EmployeeBaseMapper extends GenericBaseMapper<Employee> {
+}
